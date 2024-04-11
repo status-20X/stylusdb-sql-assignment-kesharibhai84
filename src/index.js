03d9e48dd01634cd7064bb174085a0e8d7b7a877
kesharibhai84 <vishalkeshari2004@gmail.com>
2024-04-11 23:07:44 +0530

    step6

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 // src/index.js
-
-const parseQuery = require('./queryParser');
-const readCSV = require('./csvReader');
-
-async function executeSELECTQuery(query) {
-    const { fields, table, whereClause } = parseQuery(query);
-    const data = await readCSV(`${table}.csv`);
-    
-    // Filtering based on WHERE clause
-    const filteredData = whereClause
-        ? data.filter(row => {
-            const [field, value] = whereClause.split('=').map(s => s.trim());
-            return row[field] === value;
-        })
-        : data;
-
-    // Selecting the specified fields
-    return filteredData.map(row => {
-        const selectedRow = {};
-        fields.forEach(field => {
-            selectedRow[field] = row[field];
-        });
-        return selectedRow;
-    });
+function evaluateCondition(row, clause) {
+    const { field, operator, value } = clause;
+    switch (operator) {
+        case '=': return row[field] === value;
+        case '!=': return row[field] !== value;
+        case '>': return row[field] > value;
+        case '<': return row[field] < value;
+        case '>=': return row[field] >= value;
+        case '<=': return row[field] <= value;
+        default: throw new Error(`Unsupported operator: ${operator}`);
+    }
 }
-
-module.exports = executeSELECTQuery;
